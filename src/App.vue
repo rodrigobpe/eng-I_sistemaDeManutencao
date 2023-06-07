c518e04b924150dd8efeda7fc77be7252be8f430
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-06-07 13:02:29 -0300

    feat: setup vue router and home page

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 
 <template>
   <Navbar logo="Teste"/>
+  <RouterView/>
 </template>
 
 <style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router"
+import Home from '../pages/Home.vue'
+
+const routes =[
+    {
+        path:'/home',
+        component: Home
+    }
+]
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes
+})
+
+export default router
